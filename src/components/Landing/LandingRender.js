121b8b1e0782b4e3a63733033efe9ff92f37d013
Chris Lemke <spcbrn1@gmail.com>
2017-08-12 23:11:45 -0600

    done for the night

--- src/components/Landing/LandingRender.js
+++ src/components/Landing/LandingRender.js
@@ -1,1 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+
+class LandingRender extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+    render() {
+        return(
+            <main className="landing-render-wrapper">
+                <nav className="landing-render-nav">
+                    <span className="landing-render-content-selectors">
+                        <a className="landing-render-content-anchor">
+                            Trending
+                        </a>
+                        <a className="landing-render-content-anchor">
+                            New
+                        </a>
+                    </span>
+                </nav>
+            </main>
+        )
+    }
+}
