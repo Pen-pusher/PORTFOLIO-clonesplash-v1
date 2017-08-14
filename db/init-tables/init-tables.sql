c928e52dbb0863346cb5690efc9936c2a90acc89
Chris Lemke <spcbrn1@gmail.com>
2017-08-14 14:55:12 -0600

    updated gitignore

--- db/init-tables/init-tables.sql
+++ db/init-tables/init-tables.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     authid varchar(42) UNIQUE NOT NULL,
     name varchar(64) NOT NULL,
-    username varchar(42) UNIQUE,
-    
+    username varchar(42) UNIQUE
+
 )
